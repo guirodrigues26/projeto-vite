d011b8a0191f65abeac1a7cf0c799f77218ff8d2
GUILHERME RODRIGUES CASTOR SILVA <10725582464@senacrs.edu.br>
2026-05-15 12:12:57 -0300

    mudanca

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react/jsx-runtime";
 import './App.css'
+import { Heading } from "./components/Heading";
 export function App(){
     console.log("Olá Mundo")
     return(
         <Fragment>
-           <h1>Olá mundo</h1>
-    <p>
+        <Heading/>        
+        <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
         A illo dolorem fugiat minus inventore laboriosam! Asperiores,
         nisi ex odio ducimus, consequuntur voluptates, officia error culpa cupiditate aperiam facere! Iusto, nihil!
